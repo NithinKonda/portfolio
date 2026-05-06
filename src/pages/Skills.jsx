ac12b55785c35bc3400816173008eb55b0dc864a
NithinKonda <nithinkonda142@gmail.com>
2026-05-06 23:06:12 +0530

    skill update

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,12 +1,13 @@
 import reactLogo from '../assets/reactjs.svg';
 import Clogo from '../assets/C.svg';
-import cssLogo from '../assets/css.svg';
 import djangoLogo from '../assets/django.svg';
 import gitLogo from '../assets/git.svg';
+import githubLogo from '../assets/github.svg';
 import htmlLogo from '../assets/html.svg';
 import javascriptLogo from '../assets/javascript.svg';
 import mongodbLogo from '../assets/mongodb.svg';
 import nextjsLogo from '../assets/nextjs.svg';
+import nodejsLogo from '../assets/nodejs.svg';
 import pythonLogo from '../assets/python.svg';
 import tailwindLogo from '../assets/tailwind.svg';
 import typescriptLogo from '../assets/typescript.svg';
@@ -15,14 +16,15 @@ import goLogo from '../assets/gologo.png'
 const skills = [
   { name: 'React', logo: reactLogo },
   { name: 'C', logo: Clogo },
-  { name: 'CSS', logo: cssLogo },
   { name: 'Django', logo: djangoLogo },
   { name: 'Git', logo: gitLogo },
+  { name: 'Github', logo: githubLogo },
   { name: 'Go', logo: goLogo },
   { name: 'HTML', logo: htmlLogo },
   { name: 'Javascript', logo: javascriptLogo },
   { name: 'MongoDB', logo: mongodbLogo },
   { name: 'NextJs', logo: nextjsLogo },
+  { name: 'NodeJs', logo: nodejsLogo },
   { name: 'Python', logo: pythonLogo },
   { name: 'Tailwind', logo: tailwindLogo },
   { name: 'Typescript', logo: typescriptLogo },
@@ -39,15 +41,15 @@ const Skills = () => {
         {skills.map((skill, index) => (
           <div
             key={index}
-            className="border border-white bg-black p-3 hover:bg-white hover:text-black transition-colors duration-300"
+            className="border border-white bg-black p-3 hover:bg-white transition-colors duration-300 group"
           >
             <img
               src={skill.logo}
               alt={`${skill.name} logo`}
-              className="h-10 w-10 md:h-14 md:w-12 mx-auto filter brightness-0 invert"
+              className="h-10 w-10 md:h-14 md:w-12 mx-auto filter brightness-0 invert group-hover:invert-0 transition-all duration-300"
             />
-            <div className="text-center mt-2 border-t border-white pt-2">
-              <span className="text-white font-mono text-xs md:text-sm tracking-wider">{skill.name}</span>
+            <div className="text-center mt-2 border-t border-white pt-2 group-hover:border-black">
+              <span className="text-white font-mono text-xs md:text-sm tracking-wider group-hover:text-black transition-colors duration-300">{skill.name}</span>
             </div>
           </div>
         ))}
